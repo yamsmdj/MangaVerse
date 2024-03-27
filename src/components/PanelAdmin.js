a917f126501cefe5c31eaa1c5f1755c0e7693a07
Yamsmdj <yamine.mdj@gmail.com>
2024-03-27 22:50:54 +0100

    oe

--- src/components/PanelAdmin.js
+++ src/components/PanelAdmin.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const PanelAdmin = ({ product }) => {
     return (
-        <div className='bg-indigo-950 w-10/12 mb-1 py-1'>
-            <ul className='grid grid-cols-7 text-white'>
-                <li className="text-center">{product.categorie?.name}</li>
-                <li className="text-center">{product.oeuvres?.name}</li>
-                <li className="text-center">{product.type?.name}</li>
-                <li className="text-center">{product.oeuvres?.rating}/10</li>
-                <li className="text-center">{product.prix}€</li>
-                <li className="text-center">Update</li>
-                <li className="text-center">Delete</li>
-            </ul>
-        </div>
+      <div className="bg-indigo-950 w-10/12 mb-1 py-1">
+        <ul className="grid grid-cols-7 text-white">
+          <li className="text-center">{product.categorie?.name}</li>
+          <li className="text-center">{product.oeuvres?.name}</li>
+          <li className="text-center">{product.type?.name}</li>
+          <li className="text-center">{product.oeuvres?.rating}/10</li>
+          <li className="text-center">{product.prix}€</li>
+          <NavLink to="/">
+            <li className="text-center">Update</li>
+          </NavLink>
+          <NavLink to="/">
+            <li className="text-center">Delete</li>
+          </NavLink>
+        </ul>
+      </div>
     );
 };
 
