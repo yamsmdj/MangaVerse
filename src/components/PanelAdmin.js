2f7a5d81f7e5ea9ac0d01bd90de4b8f4724b3b03
Vivien Laquay <vivien.laquay@gmail.com>
2024-03-27 19:05:04 +0100

    adminPanel.v3

--- src/components/PanelAdmin.js
+++ src/components/PanelAdmin.js
@@ -1,20 +1,30 @@
 import React from 'react';
+import { NavLink } from "react-router-dom";
+import Update from "../assets/update.svg";
+import Delete from "../assets/delete.svg";
 
 const PanelAdmin = ({ product }) => {
     return (
-        <div className='bg-indigo-950 w-10/12 mb-1 py-1'>
+        <div className='w-10/12 mb-1 py-1 bg-blackOP30'>
             <ul className='grid grid-cols-7 text-white'>
                 <li className="text-center">{product.categorie?.name}</li>
                 <li className="text-center">{product.oeuvres?.name}</li>
                 <li className="text-center">{product.type?.name}</li>
                 <li className="text-center">{product.oeuvres?.rating}/10</li>
                 <li className="text-center">{product.prix}€</li>
-                <li className="text-center">Update</li>
-                <li className="text-center">Delete</li>
+                <li className="text-center flex justify-center">
+                    <NavLink to="/dashboard" className="svg-container">
+                        <img src={Update} alt="boutique" className='w-8' />
+                    </NavLink>
+                </li>
+                <li className="text-center flex justify-center">
+                    <NavLink to="/dashboard" className="svg-container">
+                        <img src={Delete} alt="boutique" className='w-6' />
+                    </NavLink>
+                </li>
             </ul>
         </div>
     );
 };
-
 
 export default PanelAdmin;
