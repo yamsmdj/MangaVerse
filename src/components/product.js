953f1e532c6a3a21c0859117c9dae91c35a0f219
Yamsmdj <yamine.mdj@gmail.com>
2024-03-27 12:26:24 +0100

    filter success

--- src/components/product.js
+++ src/components/product.js
@@ -5,18 +5,15 @@ const product = ({product}) => {
       <div>
         <div>
           <img
-            src={`/img/manga/${product.oeuvres?.name
-              .replace(/\s+/g, "")
-              .toLowerCase()}/${product.picture}`}
+            src={`/img/manga/${product.oeuvres?.name.replace(/\s+/g, "").toLowerCase()}/${product.picture}`}
             alt={product.name}
-            className=" h-64"/>
+            className=" h-64"
+          />
           <p>{product.oeuvres?.name}</p>
           <p>{product.name}</p>
-          <p>prix : {product.prix}</p>
-          <p>{product.created_at}</p>
-          <button className=" rounded p-2 bg-blue-500">
-            Ajouter au panier
-          </button>
+          <p> prix : {product.prix}</p>
+          <p> Crée le : {product.created_at}</p>
+          <button className=" rounded p-2 bg-blue-500">Ajouter au panier</button>
         </div>
       </div>
     );
