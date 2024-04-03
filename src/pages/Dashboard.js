4b48ba65c301974278dca5e728101e69fdefe549
Yamsmdj <yamine.mdj@gmail.com>
2024-04-03 17:17:57 +0200

    dashboard ok

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -5,155 +5,166 @@ import NavAdmin from "../components/wrapper/NavAdmin";
 import OeuvresAdmin from "../components/OeuvresAdmin";
 import createV from "../assets/createGreen.svg";
 import create from "../assets/create.svg";
-import { NavLink } from "react-router-dom"; import { useLocation, useParams } from "react-router-dom"; // Import de useLocation pour récupérer les données de l'état
+import { NavLink } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom"; // Import de useLocation pour récupérer les données de l'état
 
 const Dashboard = () => {
-    const [products, setProducts] = useState([]);
-    const [oeuvres, setOeuvres] = useState([]);
-    const [selectedOeuvre, setSelectedOeuvre] = useState(null);
-    const [flashMessage, setFlashMessage] = useState(""); // Nouvelle propriété d'état pour le message flash
-    const location = useLocation();
-    const { id } = useParams(); // Utilisation de useLocation pour récupérer les données de l'état
+  const [products, setProducts] = useState([]);
+  const [oeuvres, setOeuvres] = useState([]);
+  const [selectedOeuvre, setSelectedOeuvre] = useState(null);
+  const [flashMessage, setFlashMessage] = useState(""); // Nouvelle propriété d'état pour le message flash
+  const location = useLocation();
+  const { id } = useParams(); // Utilisation de useLocation pour récupérer les données de l'état
 
-    useEffect(() => {
-        // Récupérer les produits
-        axios
-            .get(`http://localhost:8000/api/products`)
-            .then((res) => {
-                setProducts(res.data);
-            })
-            .catch((error) => {
-                console.error(
-                    "Une erreur s'est produite lors de la récupération des produits : ",
-                    error
-                );
-            });
+  useEffect(() => {
+    // Récupérer les produits
+    axios
+      .get(`http://localhost:8000/api/products`)
+      .then((res) => {
+        setProducts(res.data);
+      })
+      .catch((error) => {
+        console.error(
+          "Une erreur s'est produite lors de la récupération des produits : ",
+          error
+        );
+      });
 
-        // Récupérer les oeuvres
-        axios
-            .get(`http://localhost:8000/api/oeuvres`)
-            .then((res) => {
-                setOeuvres(res.data);
-            })
-            .catch((error) => {
-                console.error(
-                    "Une erreur s'est produite lors de la récupération des oeuvres : ",
-                    error
-                );
-            });
+    // Récupérer les oeuvres
+    axios
+      .get(`http://localhost:8000/api/oeuvres`)
+      .then((res) => {
+        setOeuvres(res.data);
+      })
+      .catch((error) => {
+        console.error(
+          "Une erreur s'est produite lors de la récupération des oeuvres : ",
+          error
+        );
+      });
 
-        // Vérifier si un message flash est passé en tant que state depuis UpdateAdmin
-        if (location.state && location.state.flashMessage) {
-            setFlashMessage(location.state.flashMessage);
-            const timeout = setTimeout(() => {
-                setFlashMessage("");
-            }, 5000);
-            return () => clearTimeout(timeout);
+    // Vérifier si un message flash est passé en tant que state depuis UpdateAdmin
+    if (location.state && location.state.flashMessage) {
+      setFlashMessage(location.state.flashMessage);
+      const timeout = setTimeout(() => {
+        setFlashMessage("");
+      }, 5000);
+      return () => clearTimeout(timeout);
+    }
+  }, []);
 
-        }
-    }, []);
+  const handleDelete = (productId) => {
+    axios
+      .delete(`http://localhost:8000/api/products/${productId}`)
+      .then((response) => {
+        setProducts(products.filter((product) => product.id !== productId));
+        setFlashMessage("Le produit a été supprimé avec succès !");
+        const timeout = setTimeout(() => {
+          setFlashMessage("");
+        }, 5000);
+        return () => clearTimeout(timeout);
+      })
+      .catch((error) => {
+        console.error(
+          "Une erreur s'est produite lors de la suppression du produit : ",
+          error
+        );
+        // Afficher un message d'erreur si la suppression échoue
+        setFlashMessage(
+          "Une erreur s'est produite lors de la suppression du produit."
+        );
+      });
+  };
 
-    const handleDelete = (productId) => {
-        axios.delete(`http://localhost:8000/api/products/${productId}`)
-            .then(response => {
-                setProducts(products.filter(product => product.id !== productId));
-                setFlashMessage("Le produit a été supprimé avec succès !");
-                const timeout = setTimeout(() => {
-                    setFlashMessage("");
-                }, 5000);
-                return () => clearTimeout(timeout);
-            })
-            .catch(error => {
-                console.error("Une erreur s'est produite lors de la suppression du produit : ", error);
-                // Afficher un message d'erreur si la suppression échoue
-                setFlashMessage("Une erreur s'est produite lors de la suppression du produit.");
-            });
-    };
+  const handleSelectOeuvre = (oeuvre) => {
+    setSelectedOeuvre(oeuvre);
+  };
 
-    const handleSelectOeuvre = (oeuvre) => {
-        setSelectedOeuvre(oeuvre);
-    };
+  const filteredProducts = (() => {
+    if (selectedOeuvre) {
+      return products.filter(
+        (product) => product.oeuvres?.id === selectedOeuvre.id
+      );
+    }
+  })();
 
-    const filteredProducts = (() => {
-        if (selectedOeuvre) {
-            return products.filter((product) => product.oeuvres?.id === selectedOeuvre.id);
-        }
-    })();
-
-    return (
-        <div className="w-full flex flex-row bg-bleuDark">
-            <NavAdmin />
-            <div className="w-full flex flex-col justify-center items-center">
-                <div className="w-full h-96 bg-bgAdmin bg-no-repeat bg-cover bg-center">
-                    <h1 className="flex h-full justify-center items-center text-white">Produits</h1>
-                </div>
-                <div className="flex justify-between w-10/12">
-                    <div className="bg-white h-fit bg-opacity-10 w-1/5 flex flex-col my-20  items-center">
-                        <div className="bg-nav opacity-100 w-10/12 mt-10 mb-1 py-1 text-white flex items-center justify-around text-center">Nouvelle oeuvres <img src={createV} alt="" /></div>
-                        <div className="w-full flex flex-col items-center mb-10">
-                          {oeuvres ? (
-                                oeuvres.map((oeuvre, index) => (
-                                    <div key={index} onClick={() => handleSelectOeuvre(oeuvre)} className="cursor-pointer w-10/12">
-                                        <OeuvresAdmin oeuvre={oeuvre} />
-                                    </div>
-                                ))
-                            ) : (
-                                <p>Chargement en cours...</p>
-                          )}
-                        </div>
-                    </div>
-
-                    <div className="bg-white bg-opacity-10 w-3/4 flex flex-col justify-start items-center my-20 text-white">
-                        <div className="flex justify-end w-full mt-6 mr-28">
-                            <NavLink to="/dashboard/create">
-                                <div className="flex p-2 gap-3 items-center bg-green-500">
-                                    <img src={create} alt="logo_create" />
-                                    Ajouter un produit
-                                </div>
-                            </NavLink>
-                        </div>
-                        {flashMessage && (
-                            <div className="bg-green-500 text-white px-4 py-2 mt-4">
-                                {flashMessage}
-                            </div>
-                        )}
-                        <div className="opacity-100 w-10/12 mt-10 mb-1 py-1">
-                            <div className="flex justify-end w-full mb-2">
-                                <NavLink to="/dashboard/create">
-                                    <div className="flex p-2 gap-3 items-center rounded-md bg-green-700">
-                                        <img src={create} alt="logo_create" />
-                                        Ajouter un produit
-                                    </div>
-                                </NavLink>
-                            </div>
-                            <ul className="bg-nav grid grid-cols-8 text-center p-2">
-                                <li>Categorie</li>
-                                <li>Oeuvre</li>
-                                <li>Nom</li>
-                                <li>Type</li>
-                                <li>Note</li>
-                                <li>Prix</li>
-                                <li>Update</li>
-                                <li>Delete</li>
-                            </ul>
-                        </div>
-                        {selectedOeuvre ? (
-                            filteredProducts.length > 0 ? (
-                                filteredProducts.map((product, index) => (
-                                    <PanelAdmin product={product} key={index} onDelete={() => handleDelete(product.id)} />
-                                ))
-                            ) : (
-                                <p>Aucun produit trouvé pour cette oeuvre.</p>
-                            )
-                        ) : (
-                            <p>Sélectionnez une oeuvre pour filtrer les produits.</p>
-                        )}
-
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="w-full flex flex-row bg-bleuDark">
+      <NavAdmin />
+      <div className="w-full flex flex-col justify-center items-center">
+        <div className="w-full h-96 bg-bgAdmin bg-no-repeat bg-cover bg-center">
+          <h1 className="flex h-full justify-center items-center text-white">
+            Produits
+          </h1>
         </div>
-    );
+        <div className="flex justify-between w-10/12">
+          <div className="bg-white h-fit bg-opacity-10 w-1/5 flex flex-col my-20  items-center">
+            <div className="bg-nav opacity-100 w-10/12 mt-10 mb-1 py-1 text-white flex items-center justify-around text-center">
+              Nouvelle oeuvres <img src={createV} alt="" />
+            </div>
+            <div className="w-full flex flex-col items-center mb-10">
+              {oeuvres ? (
+                oeuvres.map((oeuvre, index) => (
+                  <div
+                    key={index}
+                    onClick={() => handleSelectOeuvre(oeuvre)}
+                    className="cursor-pointer w-10/12"
+                  >
+                    <OeuvresAdmin oeuvre={oeuvre} />
+                  </div>
+                ))
+              ) : (
+                <p>Chargement en cours...</p>
+              )}
+            </div>
+          </div>
+
+          <div className="bg-white bg-opacity-10 w-3/4 flex flex-col justify-start items-center my-20 text-white">
+            {flashMessage && (
+              <div className="bg-green-500 text-white px-4 py-2 mt-4">
+                {flashMessage}
+              </div>
+            )}
+            <div className="opacity-100 w-10/12 mt-10 mb-1 py-1">
+              <div className="flex justify-end w-full mb-2">
+                <NavLink to="/dashboard/create">
+                  <div className="flex p-2 gap-3 items-center rounded-md bg-green-700">
+                    <img src={create} alt="logo_create" />
+                    Ajouter un produit
+                  </div>
+                </NavLink>
+              </div>
+              <ul className="bg-nav grid grid-cols-8 text-center p-2">
+                <li>Categorie</li>
+                <li>Oeuvre</li>
+                <li>Nom</li>
+                <li>Type</li>
+                <li>Note</li>
+                <li>Prix</li>
+                <li>Update</li>
+                <li>Delete</li>
+              </ul>
+            </div>
+            {selectedOeuvre ? (
+              filteredProducts.length > 0 ? (
+                filteredProducts.map((product, index) => (
+                  <PanelAdmin
+                    product={product}
+                    key={index}
+                    onDelete={() => handleDelete(product.id)}
+                  />
+                ))
+              ) : (
+                <p>Aucun produit trouvé pour cette oeuvre.</p>
+              )
+            ) : (
+              <p>Sélectionnez une oeuvre pour filtrer les produits.</p>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
